6e905fed90420fa24b4f89f6c15bc73fed7339e6
Nanar Boursalian <nanar.boursalian01@gmail.com>
2020-03-15 14:56:37 -0700

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Nanar Boursalian on 3/15/20.
+//  Copyright © 2020 NanarBoursalian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
