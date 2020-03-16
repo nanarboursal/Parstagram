beb46fcbd8f3e7373fc4a555e13d8946b75b61fa
Nanar Boursalian <nanar.boursalian01@gmail.com>
2020-03-15 17:11:58 -0700

    added all functionality, including feed and camera

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,30 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Nanar Boursalian on 3/15/20.
+//  Copyright © 2020 NanarBoursalian. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
