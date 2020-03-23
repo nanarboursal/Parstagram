0227cd39bdcd85e86acd59090331e3082157c4e7
Nanar Boursalian <nanar.boursalian01@gmail.com>
2020-03-22 20:58:52 -0700

    added commenting features! user can also stay logged in and can log out

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -12,7 +12,7 @@ import Parse
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -23,6 +23,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 configuration.server = "https://fierce-basin-48891.herokuapp.com/parse"
             })
         )
+        
+        
         
         return true
     }
